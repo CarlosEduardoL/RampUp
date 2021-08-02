b956ed60105608d5e7e638fdf3a74c0151a9fd28
CarlosEduardoL <carlos.lizalda1@hotmail.com>
2021-08-02 13:16:13 -0500

    fix on initial script

--- full-aws/scripts/setup-back.sh
+++ full-aws/scripts/setup-back.sh
@@ -2,5 +2,5 @@
 sudo apt-get update
 sudo apt-get install ansible -y
 git clone -b configuration-management https://github.com/CarlosEduardoL/RampUp
-cd RampUp/ansible || echo "fail $(ls)" && exit
+cd RampUp/ansible
 ansible -i host -l tag_Name_Movies_front_instance site.yml -vvv -e db_host=${db_host} -e db_user="root" -e db_pass=${db_pass} -e tag=${tag}
--- full-aws/scripts/setup-front.sh
+++ full-aws/scripts/setup-front.sh
@@ -2,5 +2,5 @@
 sudo apt-get update
 sudo apt-get install ansible -y
 git clone -b configuration-management https://github.com/CarlosEduardoL/RampUp
-cd RampUp/ansible || echo "fail $(ls)" && exit
+cd RampUp/ansible
 ansible -i host -l tag_Name_Movies_front_instance site.yml -vvv -e back_host=${back_host} -e tag=${tag}
