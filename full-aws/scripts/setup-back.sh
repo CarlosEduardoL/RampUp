a391dd32f0fcfb39a4fbee47550b7256870d11c4
CarlosEduardoL <carlos.lizalda1@hotmail.com>
2021-08-02 13:57:17 -0500

    fix ansible-playbook command on init script

--- full-aws/scripts/setup-back.sh
+++ full-aws/scripts/setup-back.sh
@@ -3,4 +3,4 @@ sudo apt-get update
 sudo apt-get install ansible -y
 git clone -b configuration-management https://github.com/CarlosEduardoL/RampUp
 cd RampUp/ansible
-ansible -i host -l tag_Name_Movies_front_instance site.yml -vvv -e db_host=${db_host} -e db_user="root" -e db_pass=${db_pass} -e tag=${tag}
+ansible-playbook -i host -l tag_Name_Movies_back_instance site.yml -vvv -e db_host=${db_host} -e db_user="root" -e db_pass=${db_pass} -e tag=${tag}
--- full-aws/scripts/setup-front.sh
+++ full-aws/scripts/setup-front.sh
@@ -3,4 +3,4 @@ sudo apt-get update
 sudo apt-get install ansible -y
 git clone -b configuration-management https://github.com/CarlosEduardoL/RampUp
 cd RampUp/ansible
-ansible -i host -l tag_Name_Movies_front_instance site.yml -vvv -e back_host=${back_host} -e tag=${tag}
+ansible-playbook -i host -l tag_Name_Movies_front_instance site.yml -vvv -e back_host=${back_host} -e tag=${tag}
