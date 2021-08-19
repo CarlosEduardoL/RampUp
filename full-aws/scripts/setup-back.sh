ef1b7204829cc3c62becc7f54db6d01cba6c92da
CarlosEduardoL <carlos.lizalda1@hotmail.com>
2021-08-19 14:26:05 -0500

    env file

--- full-aws/scripts/setup-back.sh
+++ full-aws/scripts/setup-back.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+{
+  echo "DB_HOST=${dns}"
+  echo "DB_USER=root"
+  echo "DB_PASS=${pass}"
+} > /home/ubuntu/.container.env
 curl ${bastion}:5555/created --header "Content-Type: application/json" --request POST --data '{"Type":1, "Dns":"${dns}"}'
